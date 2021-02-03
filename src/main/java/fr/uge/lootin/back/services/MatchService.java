3c40f51923b3cfee393b6817aa9750098ef2971a
LouisBillaut <louisbillaut2000@gmail.com>
2021-02-04 00:18:26 +0100

    fix: user don't get himself in matchs anymore

--- src/main/java/fr/uge/lootin/back/services/MatchService.java
+++ src/main/java/fr/uge/lootin/back/services/MatchService.java
@@ -24,15 +24,30 @@ public class MatchService {
         return matchRepository.save(match);
     }
 
+    private void findMatchWithGame(User actualUser, Game game, List<Match> res){
+        List<User> matchs = userRepository.findByGamesGameName(game.getGameName());
+        for (User u: matchs) {
+            if(!u.equals(actualUser)) {
+                Match m = new Match(u, actualUser);
+                matchRepository.save(m);
+                res.add(m);
+            }
+        }
+    }
+
     //TODO improve to not get all match and don't get same matchs twice
     //TODO gérer les Optionals
-    //TODO régler le bug des résultats de matchs
+    //TODO ne get que les nouveaux matchs
+    //TODO utiliser un matchDto
     public List<Match> getMatchs(User principal) {
         List<User> users = new ArrayList<>();
-        Optional<User> p = userRepository.findByLoginUsername(principal.getUsername());
-        p.get().getGames().forEach(x -> userRepository.findByGamesGameName(x.getGameName()).forEach(y -> users.add(y)));
         List<Match> matchs = new ArrayList<>();
-        users.forEach(x -> matchs.add(new Match(p.get(), x)));
+        Optional<User> p = userRepository.findByLoginUsername(principal.getUsername());
+        /*
+        p.get().getGames().forEach(x -> userRepository.findByGamesGameName(x.getGameName()).forEach(y -> users.add(y)));
+
+        users.forEach(x -> matchs.add(new Match(p.get(), x)));*/
+        p.get().getGames().forEach(x -> findMatchWithGame(p.get(), x, matchs));
         return matchs;
     }
 }
